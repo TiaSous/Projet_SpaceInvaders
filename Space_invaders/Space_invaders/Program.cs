6d5386e30e93e62e361c173266168dc5b610924b
TiaSous <122774929+TiaSous@users.noreply.github.com>
2023-09-13 08:30:25 +0200

    chore(POO): add folder PO and UX and create program c#

---
+++ Space_invaders/Space_invaders/Program.cs
@@ -0,0 +1,31 @@
+﻿using Space_invaders;
+
+Console.CursorVisible = false;
+Console.SetWindowSize(Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
+
+//récupère l'input du clavier
+ConsoleKeyInfo keyPressed;
+
+Player player = new Player();
+while (true)
+{
+    player.Update(0);
+    if (Console.KeyAvailable)
+    {
+        //lit l'input
+        keyPressed = Console.ReadKey(true);
+        switch (keyPressed.Key)
+        {
+            case ConsoleKey.A:
+                player.Update(-1);
+                break;
+            case ConsoleKey.D:
+                player.Update(1);
+                break;
+        }
+    }
+
+    Console.Clear();
+    player.Show();
+    Thread.Sleep(50);
+}
