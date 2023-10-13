e73a2ff5f5dfe01de3b04eff57fddc661412fe9b
TiaSous <tiago.rodrigues2@eduvaud.ch>
2023-10-13 11:22:11 +0200

    Doc(POO): add analyse fonctionnel and analyse technique

---
+++ POO/SpicyConso/ModelTests/PlayerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class PlayerTests
+    {
+        [TestMethod()]
+        public void PlayerMovementUpdateTest()
+        {
+            Player player = new Player();
+            int ancienX = player._x;
+            player.PlayerMovementUpdate(1 * player.Speed);
+            Assert.AreEqual(player._x, ancienX + player.Speed);
+            ancienX = player._x;
+            player.PlayerMovementUpdate(-1 * player.Speed);
+            Assert.AreEqual(player._x, ancienX - player.Speed);
+        }
+    }
+}
